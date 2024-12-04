0c81eb48548cb9f5b3636560d4694745993c61f1
root <root@4nxietz.4nxietz>
2024-12-04 03:04:32 -0500

    rizal commit

---
+++ login_admin.php
@@ -0,0 +1,53 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Booking Lapangan futsal</title>
+
+    <!-- Custom fonts for this template-->
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link
+        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet">
+
+    <!-- Custom styles for this template-->
+    <link href="css/sb-admin-2.min.css" rel="stylesheet">
+
+</head>
+<body>
+    <div style="height: 100vh;background-color: #eaeaea;" class="container-fluid d-flex justify-content-center align-items-center">
+    <div class="container-fluid d-flex justify-content-center align-items-center">
+        <div class="card" style="width: 50rem;">
+            <div class="card-body ">
+              <h5 class="card-title text-center">Login Admin</h5>
+                <div class="container">
+                        <form>
+                                <img src="img/login.jpg">
+                                <div class="mb-3">
+                                  <label for="nama" class="form-label">Nama</label>
+                                  <input type="text" class="form-control" id="nama" aria-describedby="emailHelp">
+                                </div>
+                                  <div class="mb-3">
+                                    <label for="subject" class="form-label">password</label>
+                                    <input type="password" class="form-control" id="password" aria-describedby="emailHelp">
+                                  </div>
+                                
+                                <button type="submit" class="btn btn-primary form-control">Login</button>
+                        </form>
+                </div>
+            </div>     
+        </div>
+        
+    </div>
+    </div>
+</body>
+
+</html>
